b885c9eb154b469aa82eb002bff41c88d151de2f
Loacking <LudovicDop@gmail.com>
2025-01-28 10:45:34 +0100

    big maj

---
+++ README.md
@@ -0,0 +1,1 @@
+# SEJOUR_ACCESSIBLE.io
--- src/script.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.category').forEach(category => {
-    category.addEventListener('click', () => {
-        alert(`Vous avez cliqué sur : ${category.querySelector('h3').textContent}`);
-    });
-});
